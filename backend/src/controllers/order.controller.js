a65577e5f5f46e50e0c1c8446bf3c8fa30d2a7b4
Anderson Garcia <ajheremy62@gmail.com>
2025-07-21 20:41:06 -0500

    controlador ordenes

---
+++ backend/src/controllers/order.controller.js
@@ -0,0 +1,40 @@
+import OrderService from '../services/order.service.js';
+
+export const confirmarPedido = async (req, res, next) => {
+  try {
+    const pedido = await OrderService.createFromCart(req.user.id);
+    res.status(201).json(pedido);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const actualizarPago = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const pedidoActualizado = await OrderService.markAsPaid(id);
+    res.status(200).json(pedidoActualizado);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getPedidosPorUsuario = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const pedidos = await OrderService.getByUserId(id);
+    res.status(200).json(pedidos);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const eliminarPedido = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await OrderService.deleteById(id);
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+};
